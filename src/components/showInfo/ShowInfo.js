1a6daa15b9415ebb3ac6611a32f781f4a8141c79
Luis Pablo Serrano <superpato1234@gmail.com>
2024-08-07 20:42:15 -0600

    Revert "fix: add use state hook forhandle type"

--- src/components/showInfo/ShowInfo.js
+++ src/components/showInfo/ShowInfo.js
@@ -1,9 +1,8 @@
-import React,{useState,useEffect} from 'react'
+import React from 'react'
 import './ShowInfo.css'
 
 const ShowInfo = (props) => {
 
-    const [pokeType, setPokeType] = useState('')
     const {pokeInfo} = props;
     let url = pokeInfo?.sprites;
     url===undefined?url="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png":url=pokeInfo.sprites.front_default;
@@ -14,22 +13,19 @@ const ShowInfo = (props) => {
             for(const type in pokeInfo.types){
                 types+=pokeInfo.types[type].type.name+' '
             }
-            setPokeType(types);
+            return <p className='stats'>type: {types}</p>
         }
         else{
-            setPokeType('none');
+            return <p className='stats'>none</p>
         }
     }
 
-    useEffect(()=>{
-        type();
-    },[pokeInfo])
     return (
         <div>
             <p className='NamePoke'>{pokeInfo.name}</p>
             <div className='ContainerImg'>
                 <img src={url} alt="" className='ImagePoke'></img>
-                <p className='stats'>{pokeType}</p>
+                {type()}
             </div>
         </div>
     )
